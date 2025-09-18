57f6eebd7d9949847aeb6c6baae8c599b54e4dbd
mannbella <135073819+mannbella@users.noreply.github.com>
2025-09-18 09:59:15 -0700

    updates

--- proj.py
+++ proj.py
@@ -1,14 +1,20 @@
 import csv
 import pandas as pd
+import numpy as np
 
-tabulationSheetOne = pd.read_csv('results_report (4).csv')
+tabulationSheetOne = pd.read_csv('results_report_(4).csv')
 tabulationSheetOneFixed = tabulationSheetOne.drop('List', axis=1)
 
 columnsToKeep = ['Council ID', 'First Name', 'Last Name', 'Overall', 'AOII Interest (0 - Standard Round)', 'Ambition (0 - Standard Round)', 'Likability (0 - Standard Round)' ]
+columnsToRound = ['Overall', 'AOII Interest (0 - Standard Round)', 'Ambition (0 - Standard Round)', 'Likability (0 - Standard Round)']
 
 newSheet = tabulationSheetOneFixed[columnsToKeep]
 filteredSheet = newSheet[newSheet['Overall'] != 0]
 filteredSheet = filteredSheet.sort_values(by='Overall', ascending=False)
-filteredSheet.to_csv('tabulationOutput1.csv', index=False) # CHANGE THE INDEX EACH TIME YOU RUN THE PROG
+
+for col in columnsToRound:
+    filteredSheet[col] = np.sign(filteredSheet[col]) * np.floor(np.abs(filteredSheet[col]) + 0.5)
+
+filteredSheet.to_csv('tabulationOutput2.csv', index=False) # CHANGE THE INDEX EACH TIME YOU RUN THE PROG
 
 print(filteredSheet.head(10))
